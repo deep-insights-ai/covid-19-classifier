76e9386e256061d87ced5a4d1ceb69bbb9f5ed06
MillivoltLightningbolt <h.kaplan@gmx.net>
2020-04-24 14:46:34 +0200

    pull request fixes: - added missing constants - mentioned that model requires functions for loading during inference - added main function to demonstrate classifying an image (TODO: add   actual image)

--- v1.0/inference.py
+++ v1.0/inference.py
@@ -3,7 +3,13 @@ from fastai2.vision.all import *
 # REPLACE THIS MODEL WITH MOST UP TO DATE VERSION
 MODEL = 'rn50-448px-v0.9-export.pkl'
 
+LBL_PATHO = 'Non-COVID'
+LBL_COV = 'COVID'
 
+
+# when loading the model for inference, the fastai learner requires
+# that the functions which were used during training exist
+# in the same namespace, even if not used, hence the definition here
 def get_lbl(fname):
     """return the label of given image"""
     lbl = fname.parent.name
@@ -13,7 +19,7 @@ def get_lbl(fname):
         return LBL_COV
     else:
         raise ValueError(f'could not extract label from {fname}')
-        
+
 
 def in_valid(fname):
     """return True if file in validationset, else False"""
@@ -22,7 +28,7 @@ def in_valid(fname):
 
 LEARN = load_learner(MODEL)
 
-CATEGORIES = LEARN.dls.vocab # classes in order of output neurons
+CATEGORIES = LEARN.dls.vocab  # classes in order of output neurons
 
 
 def predict(img):
@@ -32,3 +38,9 @@ def predict(img):
     y_hat, idx, probs = LEARN.predict(img)
     probabilities = [p.item() for p in probs]
     return dict(zip(CATEGORIES, probabilities))
+
+
+if __name__ == "__main__":
+    img = '<TODO: add img path to img in repo, (relative path names are great)'
+    print(f'classifying image {img}:')
+    print(predict(img))
